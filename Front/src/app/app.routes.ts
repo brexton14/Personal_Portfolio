bf88a1626e5ba1251049801b4f8e27e50bc40c5c
bth1570 <bth1570@wgu.edu>
2025-05-13 10:39:32 -0400

    Building of HTML main files, linking and starting log in page

--- Front/src/app/app.routes.ts
+++ Front/src/app/app.routes.ts
@@ -1,3 +1,18 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { PortfolioComponent } from './pages/portfolio/portfolio.component';
+import { ChartsComponent } from './pages/charts/charts.component';
+import { LoginComponent } from './pages/login/login.component';
+
+export const routes: Routes = [
+  {path: 'portfolio', component: PortfolioComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'charts', component: ChartsComponent},
+  {path: '', redirectTo: '/login', pathMatch: 'full'},
+];
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
