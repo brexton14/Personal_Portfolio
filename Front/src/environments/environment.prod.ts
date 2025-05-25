871930cdeca7ec5e543e8dbbc02caf24514440fb
bth1570 <bth1570@wgu.edu>
2025-05-25 09:58:32 -0400

    Revert Back

---
+++ Front/src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  apiBaseUrl: 'http://localhost:8080/api',
+  s3AssetsUrl: 'http://localhost:4200/assets',
+  enableAnalytics: false,
+  enableErrorLogging: false
+};
