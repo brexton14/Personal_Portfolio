d1d32e95be19d1c7cc54fd38b4f7bd1824685cfe
bth1570 <bth1570@wgu.edu>
2025-05-26 15:06:16 -0400

    Connection of database

--- src/main/java/com/system/portfolio/controllers/ScraperController.java
+++ src/main/java/com/system/portfolio/controllers/ScraperController.java
@@ -8,6 +8,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/scraper")
+@CrossOrigin(origins = "https://thebrextonexperience.com/")
 public class ScraperController {
 
     @PostMapping
