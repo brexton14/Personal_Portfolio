c4fb6d67bdd8c5f9c3e0b5efc0e42a80509dfda2
bth1570 <bth1570@wgu.edu>
2025-05-28 14:49:01 -0400

    healthcontroller added

---
+++ src/main/java/com/system/portfolio/controllers/HealthController.java
@@ -0,0 +1,13 @@
+package com.system.portfolio.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+    @GetMapping("/")
+    public String health() {
+        return "OK";
+    }
+}
+
