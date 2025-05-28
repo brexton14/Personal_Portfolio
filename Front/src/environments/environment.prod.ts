fefd9c94af9db25330408a10602e5d56c4ab5646
bth1570 <bth1570@wgu.edu>
2025-05-28 01:02:49 -0400

    finishing touches

--- Front/src/environments/environment.prod.ts
+++ Front/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://springbackbrex.us-east-1.elasticbeanstalk.com/api',
+  apiBaseUrl: 'BrextonPortBack.us-east-1.elasticbeanstalk.com/api',
   s3AssetsUrl: 'https://thebrextonexperience.com/',
   enableAnalytics: true,
   enableErrorLogging: true
